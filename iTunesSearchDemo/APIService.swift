820613bc11a427989cf66d3885be2c063c97638a
Sachin Singla <sk942593@gmail.com>
2021-04-06 23:13:26 +0530

    Code cleaned.

--- iTunesSearchDemo/APIService.swift
+++ iTunesSearchDemo/APIService.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import Combine
 class APIServices: NSObject {
     static let shared = APIServices()
+    private let baseURL = "https://itunes.apple.com/search?term="
+    var songInfo: SongInfo?
     func fetchSongs(term: String, completionHandler: @escaping (SongInfo) -> Void){
-        guard let safeURL = URL(string: "https://itunes.apple.com/search?term=\(term)") else {return}
+        guard let safeURL = URL(string: "\(baseURL)\(term)") else {return}
         URLSession.shared.dataTask(with: safeURL) { (data, res, err) in
             if let error = err{
                 print(error.localizedDescription)
